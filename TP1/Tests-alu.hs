337476343d3aff6f0afb0a916589bfa7007de1aa
nico <nicoroulet@gmail.com>
2016-04-09 16:43:42 -0300

    tests hasta extraer features

--- TP1/Tests-alu.hs
+++ TP1/Tests-alu.hs
@@ -11,7 +11,11 @@ main = runTestTT allTests
 allTests = test [
  	"split" ~: testsSplit,
  	"longitudPromedioPalabras" ~: testsLongitudPromedioPalabras, 
- 	"cuentas" ~: testsCuentas
+ 	"cuentas" ~: testsCuentas,
+ 	"repeticionesPromedio" ~: testRepeticionesPromedio,
+ 	"frecuenciaTokens" ~: testFrecuenciaTokens,
+ 	"normalizarExtractor" ~: testNormalizarExtractor,
+ 	"extraerFeatures" ~: testExtraerFeatures
  	]
 
 testsSplit = test [
@@ -26,4 +30,21 @@ testsLongitudPromedioPalabras = test [
 
 testsCuentas = test [
 	cuentas ["x","x","y","x","z"] ~?= [(3,"x"), (1,"y"), (1,"z")]
+	]
+
+testRepeticionesPromedio = test [
+	repeticionesPromedio "hola chau hola a hola hola" ~?= 2,
+	repeticionesPromedio "a a a a a" ~?= 5
+	]
+	
+testFrecuenciaTokens = test [
+	(head frecuenciaTokens) "_a_b_c_d" ~?= 0.5
+	]
+	
+testNormalizarExtractor = test [
+	(normalizarExtractor ["abc abcd ab", "a", "abcd"] longitudPromedioPalabras) "abc abcd abcde" ~?= 1 -- normalizar divide por 4 en este caso
+	]
+
+testExtraerFeatures = test [
+	extraerFeatures [longitudPromedioPalabras, repeticionesPromedio] ["abc abc abcdefg abc", "a b c d"] ~?= [[1.0, 1.0],[0.25, 0.5]] -- frecuencias y repeticiones despues de normalizar
 	]
